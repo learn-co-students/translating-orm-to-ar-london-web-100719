e09760cc43bd83645d09fccaeee2c2e610439d43
Ned-el-ch <nedelchev.niki@gmail.com>
2019-10-17 20:51:23 +0100

    halfway done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,19 @@
-class Dog
+require 'pry'
+
+class Dog < ActiveRecord::Base
+
+    attr_accessor :name, :breed, :id
+
+    def initialize(attributes)
+
+        attributes.each {|key, value| self.send("#{key}=", value)}
+        
+    end
+
+    def self.create(attributes)
+    binding.pry
+        Dog.new(attributes)
+        
+    end
+    
 end
