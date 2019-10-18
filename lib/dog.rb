28cab3786cc4848a3427ff13d87845f7f5922272
Ned-el-ch <nedelchev.niki@gmail.com>
2019-10-18 07:57:38 +0100

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,18 +2,6 @@ require 'pry'
 
 class Dog < ActiveRecord::Base
 
-    attr_accessor :name, :breed, :id
-
-    def initialize(attributes)
-
-        attributes.each {|key, value| self.send("#{key}=", value)}
-        
-    end
-
-    def self.create(attributes)
-    binding.pry
-        Dog.new(attributes)
-        
-    end
+    # attr_accessor :name, :breed, :id
     
 end
